88f91aec563818ef39fc6cf840daca209ab63e87
shifa-m <shifamahreen786@gmail.com>
2026-01-12 19:47:07 +0530

    components & props

--- vite-project/src/components/Card.jsx
+++ vite-project/src/components/Card.jsx
@@ -1,31 +1,37 @@
 import React from 'react'
 import {Bookmark} from 'lucide-react'
 
-const Card = () => {
+const Card = (props) => {
+ 
+
   return (
    
     <div className="parent">
       <div className='card'>
         <div className="top">
-          <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSFkkQhwA-14yQus_rij6VonTP5UT7IrorBkg&s" alt="my photo" />
+          <img src={props.brandlogo} alt="my photo" />
           <button >Save <Bookmark size={12} /></button>
           </div>
           <div className="center">
-            <h3>Amazon<span>5 Days ago</span></h3>
-            <h2>Senior UI/UX Designer</h2>
+            <h3>{props.company}<span>{props.posted}</span></h3>
+            <h2>{props.role}</h2>
             <div className='tag'>
-              <h4>Part Time</h4>
-              <h4>Senior Level</h4>
+              <h4>{props.hours}</h4>
+              <h4>{props.Type}</h4>
             </div>
           </div>
           <div className="bottom">
-              <h3>$120/hr</h3>
-                <p>Bangalore,india</p>
-                 <button>Apply now</button>
+              <h3>{props.pay}</h3>
+                <p>{props.location}
+                  <button>Apply now</button>
+                </p>
+                
+                 
                   
                 </div>
           </div>
            </div>
+           
            
             
 
